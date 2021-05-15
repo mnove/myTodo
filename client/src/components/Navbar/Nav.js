9d33510081c4d9a1aa772e3bd529898239da822a
mnove <novelli3000@gmail.com>
2021-05-15 09:22:19 +0200

    remove old navbar as no longer necessary for navigation

--- client/src/components/Navbar/Nav.js
+++ client/src/components/Navbar/Nav.js
@@ -7,97 +7,74 @@ import {
   EuiHeaderLinks,
   EuiHeaderLink,
   EuiHeaderSectionItemButton,
-  EuiBadge, EuiIcon, EuiAvatar
-} from '@elastic/eui';
-
-
+  EuiBadge,
+  EuiIcon,
+  EuiAvatar,
+} from "@elastic/eui";
 
 // redux
 import { connect } from "react-redux";
 import { logoutUser } from "../../redux/index";
 
- const Nav = (props) => {
+const Nav = (props) => {
   const handleLogout = async () => {
     await props.logoutUser();
-     await props.history.push("/login");
+    await props.history.push("/login");
   };
 
   return (
-
     <>
-    <Fragment>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-              <Link className="nav-link" to="/login">
-                Login
-              </Link>
-              <Link className="nav-link" to="/register">
-                Register
-              </Link>
-              <Link className="nav-link" to="/dashboard">
-                Dashboard
-              </Link>
-              <Link className="nav-link" to="/test">
-                Test Component
-              </Link>
-
-              <Link className="nav-link" to="/login" onClick={handleLogout}>
-                Logout
-              </Link>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </Fragment>
-
-    <Fragment>
-    <EuiHeader
-    theme="default"
-    sections={[
-      {
-        items: [
-          // <EuiHeaderLogo>myTodo</EuiHeaderLogo>,
-          <EuiHeaderLinks aria-label="App navigation dark theme example">
-            <EuiHeaderLink><Link className="nav-link" to="/">
-                Home
-              </Link></EuiHeaderLink>
-            <EuiHeaderLink><Link className="nav-link" to="/login">
-                Login
-              </Link></EuiHeaderLink>
-            <EuiHeaderLink><Link className="nav-link" to="/register">
-                Register
-              </Link></EuiHeaderLink>
-            <EuiHeaderLink><Link className="nav-link" to="/dashboard">
-                Dashboard
-              </Link></EuiHeaderLink>
-            <EuiHeaderLink><Link className="nav-link" to="/test">
-                Test Component
-              </Link></EuiHeaderLink>
-            <EuiHeaderLink><Link className="nav-link" to="/login" onClick={handleLogout}>
-                Logout
-              </Link></EuiHeaderLink>
-          </EuiHeaderLinks>,
-        ],
-        borders: 'right',
-      },
-      {
-        items: [
-
-          <EuiHeaderSectionItemButton aria-label="Account menu">
-            <EuiAvatar name="Voula Ena" size="s" />
-          </EuiHeaderSectionItemButton>,
-        ],
-        borders: 'none',
-      },
-    ]}
-  />
-    </Fragment>
-
+      <EuiHeader
+        theme="default"
+        sections={[
+          {
+            items: [
+              // <EuiHeaderLogo>myTodo</EuiHeaderLogo>,
+              <EuiHeaderLinks aria-label="App navigation dark theme example">
+                <EuiHeaderLink>
+                  <Link className="nav-link" to="/">
+                    Home
+                  </Link>
+                </EuiHeaderLink>
+                <EuiHeaderLink>
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
+                </EuiHeaderLink>
+                <EuiHeaderLink>
+                  <Link className="nav-link" to="/register">
+                    Register
+                  </Link>
+                </EuiHeaderLink>
+                <EuiHeaderLink>
+                  <Link className="nav-link" to="/dashboard">
+                    Dashboard
+                  </Link>
+                </EuiHeaderLink>
+                <EuiHeaderLink>
+                  <Link className="nav-link" to="/test">
+                    Test Component
+                  </Link>
+                </EuiHeaderLink>
+                <EuiHeaderLink>
+                  <Link className="nav-link" to="/login" onClick={handleLogout}>
+                    Logout
+                  </Link>
+                </EuiHeaderLink>
+              </EuiHeaderLinks>,
+            ],
+            borders: "right",
+          },
+          {
+            items: [
+              <EuiHeaderSectionItemButton aria-label="Account menu">
+                <EuiAvatar name="Voula Ena" size="s" />
+              </EuiHeaderSectionItemButton>,
+            ],
+            borders: "none",
+          },
+        ]}
+      />
     </>
   );
 };
@@ -112,4 +89,4 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 // connect react components to Redux store and React Router
- export default withRouter(connect(null, mapDispatchToProps)(Nav));
+export default withRouter(connect(null, mapDispatchToProps)(Nav));
