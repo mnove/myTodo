f16eea481b1cdfcd9ba38a1f541bf4431aad79ea
mnove <novelli3000@gmail.com>
2021-05-19 18:54:48 +0200

    add comments to database.sql file, remove useless packages from package.json (server and client), updated keys.js file in config/keys

--- db.js
+++ db.js
@@ -1,28 +1,24 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
-
-
+const keys = require("./config/keys"); // import keys from keys.js
 
 const devConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
+  user: keys.postgresDb.user,
+  password: keys.postgresDb.password,
+  host: keys.postgresDb.host,
+  port: keys.postgresDb.port,
+  database: keys.postgresDb.database,
 };
 
 const productionConfig = {
   connectionString: process.env.DATABASE_URL,
   ssl: {
-        rejectUnauthorized: false
-      }
-}
+    rejectUnauthorized: false,
+  },
+};
 
-const pool = new Pool (
-  process.env.NODE_ENV === "production" ? productionConfig : devConfig,
-)
-
-
-
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? productionConfig : devConfig
+);
 
 module.exports = pool;
